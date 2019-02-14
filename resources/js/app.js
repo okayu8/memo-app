796311a91a9c1ce2975d9b22c0209e2bf9937cb3
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-02-14 23:50:20 +0900

    add redux

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/MemoApp');
+require('./index');
 
 $.ajaxSetup({
     headers: {
